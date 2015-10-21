ad98bdc777aa9802cfab36a542084c0ae5e2a0c6
Sergey Seitov <sseitov@gmail.com>
2015-10-21 15:27:33 +0300

    fix

--- Megatron-client/client.cpp
+++ Megatron-client/client.cpp
@@ -303,7 +303,7 @@ void Client::setButton(bool checked)
             int value = checked ? 999 : 1;
             p1.insert("Value", value);
             list.append(p1);
-        } else if (mCurrentMode == 1) {
+        } else if (mCurrentMode == 0) {
             QVariantMap p1;
             p1.insert("Node", 1);
             p1.insert("Port", port);
